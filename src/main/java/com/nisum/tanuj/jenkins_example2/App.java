fc10d5e584928645dccb2e417250397a3238327b
pbejuga <pbejuga@gap.com>
2019-05-27 11:45:05 +0530

    first commit

---
+++ src/main/java/com/nisum/tanuj/jenkins_example2/App.java
@@ -0,0 +1,13 @@
+package com.nisum.tanuj.jenkins_example2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
